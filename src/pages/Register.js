e23c1be31d465d7aa8b5d2804e7f3ec0c57111a9
Ishan Jain <56102033+ishanjain1502@users.noreply.github.com>
2022-04-01 18:58:42 +0530

    Update Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -12,7 +12,7 @@ function App() {
 	async function registerUser(event) {
 		event.preventDefault()
 
-		const response = await fetch('http://localhost:1337/api/register', {
+		const response = await fetch('', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
